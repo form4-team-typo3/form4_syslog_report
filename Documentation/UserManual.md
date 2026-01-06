68cc85c9a91e153bea24c77c6d6a1bcdc51293ec
form4 GmbH <typo3@form4.de>
2026-01-06 16:14:35 +0000

    Release 12.4.1

---
+++ Documentation/UserManual.md
@@ -0,0 +1,3 @@
+# Users manual
+
+There is nothing to do for users. The tasks needs to be implemented by an administrator.
---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'form4 SysLog Report',
+    'description' => 'Sends e-mail reports about sys_log entries.',
+    'category' => 'be',
+    'author' => 'form4 GmbH',
+    'author_company' => 'form4 GmbH',
+    'author_email' => 'typo3@form4.de',
+    'state' => 'stable',
+    'version' => '12.4.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-12.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+);
